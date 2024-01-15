774a64d69438e26c0c3c300970978eeeb1964a63
Ramakrushna Biswal <ramakrushnabunty@gmail.com>
2024-01-15 12:43:43 +0530

    all set

--- script.js
+++ script.js
@@ -1,14 +1,36 @@
 function enlargeImage(imgElement) {
+    var body = document.body;
     var popupContainer = document.getElementById('popup-container');
     var popupImg = document.getElementById('popup-img');
 
     popupImg.src = imgElement.src;
     popupImg.alt = imgElement.alt;
 
+    body.classList.add('enlarged');
     popupContainer.style.display = 'block';
 }
 
 function closePopup() {
+    var body = document.body;
     var popupContainer = document.getElementById('popup-container');
+
+    body.classList.remove('enlarged');
     popupContainer.style.display = 'none';
 }
+
+
+
+const slider = document.getElementById('carousel');
+    let currentIndex = 0;
+
+    function nextSlide() {
+        currentIndex = (currentIndex + 1) % 5;
+        updateSlider();
+    }
+
+    function updateSlider() {
+        const translateValue = -currentIndex * 100 + '%';
+        slider.style.transform = 'translateX(' + translateValue + ')';
+    }
+
+    setInterval(nextSlide, 3000);
